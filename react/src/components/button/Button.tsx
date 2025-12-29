46912c395ac6a6037b20c41165e519f2ebc99896
Salauddin Omar Sifat <mahi@workstation.local>
2025-12-29 13:34:19 +0600

    feat: Button component

--- react/src/components/button/Button.tsx
+++ react/src/components/button/Button.tsx
@@ -3,21 +3,21 @@ import styles from "./Button.module.css";
 
 export type IButtonProps = ButtonBase.Props & {
   variant?: "outline" | "ghost" | "link";
-  color?: "primary" | "secondary" | "danger";
+  color?: "secondary" | "danger";
   size?: "sm" | "lg" | "icon" | "icon-sm" | "icon-lg";
   shape?: "pill" | "circle" | "sharp";
 };
-export const Button: React.FC<IButtonProps> = (props) => {
-  const {
-    size = "default",
-    variant = "default",
-    shape = "default",
-    color = "primary",
-    ...rest
-  } = props;
+export const Button: React.FC<IButtonProps> = ({
+  variant,
+  color = "primary",
+  size,
+  shape,
+  className,
+  ...rest
+}) => {
   return (
     <ButtonBase
-      className={styles.Button}
+      className={styles.Button + " " + className}
       data-size={size}
       data-variant={variant}
       data-shape={shape}
--- react/src/stories/Button.stories.tsx
+++ react/src/stories/Button.stories.tsx
@@ -1,10 +1,11 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { Button } from "../components/button/Button";
-import { IconHome } from "@tabler/icons-react";
+import { IconHome, IconLoader } from "@tabler/icons-react";
 
 const meta = {
   component: Button,
   title: "Button",
+  tags: ["autodocs"],
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -27,6 +28,15 @@ export const Variants: Story = {
   ),
 };
 
+export const Colors: Story = {
+  render: () => (
+    <div className="showcase">
+      <Button>Default</Button>
+      <Button color="secondary">Secondary</Button>
+      <Button color="danger">Danger</Button>
+    </div>
+  ),
+};
 export const Sizes: Story = {
   render: () => (
     <div className="showcase">
@@ -36,17 +46,6 @@ export const Sizes: Story = {
     </div>
   ),
 };
-
-export const Colors: Story = {
-  render: () => (
-    <div className="showcase">
-      <Button color="primary">Primary</Button>
-      <Button color="secondary">Secondary</Button>
-      <Button color="danger">Danger</Button>
-    </div>
-  ),
-};
-
 export const IconSizes: Story = {
   render: () => (
     <div className="showcase">
@@ -67,10 +66,58 @@ export const Shapes: Story = {
   render: () => (
     <div className="showcase">
       <Button>Default</Button>
-      <Button shape="pill">Pill shaped</Button>
+      <Button shape="pill">Pill Shaped</Button>
       <Button shape="sharp">Sharp</Button>
       <Button size="icon" shape="circle">
         <IconHome />
+      </Button>
+    </div>
+  ),
+};
+
+export const States: Story = {
+  render: () => (
+    <div className="showcase">
+      <style>
+        {`
+          .spinner {
+            animation: spin 2s linear infinite;
+          }
+          @keyframes spin {
+            from {
+              transform: rotate(0deg); /* Starting angle */
+            }
+            to {
+              transform: rotate(360deg); /* Ending angle (one full turn) */
+            }
+          }
+        `}
+      </style>
+      <Button>Default</Button>
+      <Button disabled>Disabled</Button>
+      <Button disabled>
+        <IconLoader className="spinner" />
+        Loading
+      </Button>
+    </div>
+  ),
+};
+
+export const Customization: Story = {
+  render: () => (
+    <div className="showcase">
+      <style>
+        {`
+          .custom {
+            width: 200px;
+          }
+        `}
+      </style>
+      <Button
+        style={{ backgroundColor: "purple", color: "white" }}
+        className="custom"
+      >
+        Custom Styled
       </Button>
     </div>
   ),
